2205fd4aef61a0e2a9086f14256f944a3b8f431d
Tisizi <git@tisizi.de>
2023-03-07 11:36:57 +1000

    Upgrade imports

--- schema/validators.ts
+++ schema/validators.ts
@@ -1,4 +1,4 @@
-import { array, Schema, type Type, unknown } from "computed-types/index.ts";
+import { array, Schema, type Type, unknown } from "computed-types";
 export const text = unknown.string();
 export const uuid = text.regexp(/^[0-9a-f]{8}(-?[0-9a-f]{4}){3}-?[0-9a-f]{12}/);
 export const latitude = unknown.number().gte(-90).lte(90);
