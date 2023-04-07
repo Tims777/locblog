d5e7db35034154d3533172ad3eadb05f8283371f
Tisizi <git@tisizi.de>
2023-04-07 22:09:34 +0800

    Update dependencies

--- helpers/twind-helpers.ts
+++ helpers/twind-helpers.ts
@@ -1,4 +1,4 @@
-import { type Options } from "$fresh/plugins/twind.ts";
+import { type Options } from "$fresh/plugins/twindv1.ts";
 import { defineConfig } from "twind";
 import presetTailWind from "twind-preset-tailwind";
 import presetTypography from "twind-preset-typography";
