b59087ce3f47093c938209b019733104860a81aa
Tisizi <git@tisizi.de>
2023-04-10 17:13:46 +0800

    Fix PopupContainer styling

--- components/FullScreen.tsx
+++ components/FullScreen.tsx
@@ -1,5 +1,7 @@
 import { ComponentChildren } from "preact";
 
 export default function FullScreen(props: { children: ComponentChildren }) {
-  return <div class="w-screen h-screen absolute top-0 left-0">{props.children}</div>;
+  return (
+    <div class="w-screen h-screen absolute top-0 left-0">{props.children}</div>
+  );
 }
--- components/PopupContainer.tsx
+++ components/PopupContainer.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 export default function PopupContainer(props: ContainerProps) {
   return (
-    <div style="background-color: rgba(255,255,255,0.9); padding: 5px; border-radius: 0px 10px 10px;">
+    <div class="not-prose p-2 bg-white bg-opacity-90 p-1 rounded-xl rounded-tl-none">
       {props.children}
     </div>
   );
