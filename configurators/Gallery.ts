6446dfd55e795ce0b320aff3abad47105499803a
Tisizi <git@tisizi.de>
2023-04-20 22:45:58 +0200

    Auto format

--- configurators/Gallery.ts
+++ configurators/Gallery.ts
@@ -1,6 +1,6 @@
 import { type Directive } from "preactify-markdown/types.d.ts";
 import { type GalleryProps } from "../islands/Gallery.tsx";
-import {remove} from "unist-util-remove";
+import { remove } from "unist-util-remove";
 
 export default function configure(directive: Directive): GalleryProps | false {
   // TODO: Load gallery content here
--- services/redirect.ts
+++ services/redirect.ts
@@ -1,29 +1,28 @@
 class RedirectService {
+  private redirects: [URLPattern, string][] = [];
 
-    private redirects: [URLPattern, string][] = [];
-
-    constructor(redirects: string | undefined = Deno.env.get("REDIRECTS")) {
-        if (redirects) {
-            for (const [from, to] of JSON.parse(redirects)) {
-                this.redirects.push([new URLPattern(from), to]);
-            }
-        }
+  constructor(redirects: string | undefined = Deno.env.get("REDIRECTS")) {
+    if (redirects) {
+      for (const [from, to] of JSON.parse(redirects)) {
+        this.redirects.push([new URLPattern(from), to]);
+      }
     }
+  }
 
-    public for(url: string): Response | null {
-        for (const [pattern, to] of this.redirects) {
-            const result = pattern.exec(url);
-            if (result) {
-              const groups = result.pathname.groups;
-              const target = to.replace(
-                /\$([0-9]+)/,
-                (_, index) => index in groups ? groups[index] : "",
-              );
-              return Response.redirect(target, 301);
-            }
-        }
-        return null;
+  public for(url: string): Response | null {
+    for (const [pattern, to] of this.redirects) {
+      const result = pattern.exec(url);
+      if (result) {
+        const groups = result.pathname.groups;
+        const target = to.replace(
+          /\$([0-9]+)/,
+          (_, index) => index in groups ? groups[index] : "",
+        );
+        return Response.redirect(target, 301);
+      }
     }
+    return null;
+  }
 }
 
 const redirect = new RedirectService();
