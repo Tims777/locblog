9ce0c587c434437cde9f25f080aa72a15c71ee5a
Tisizi <git@tisizi.de>
2023-06-03 12:13:17 +0200

    Expand author circles on hover or focus

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -7,16 +7,38 @@ export interface CommentProps {
 }
 
 export default function Comment(props: CommentProps) {
+  const expandingAuthorCircleClasses = [
+    "absolute",
+    "left-0",
+    "h-10",
+    "leading-10",
+    "px-3",
+    "whitespace-nowrap",
+    "overflow-hidden",
+    "max-w-[2.5rem]",
+    "group-focus-within:max-w-[25rem]",
+    "group-hover:max-w-[25rem]",
+    "transition-[max-width]",
+    "bg-gray-300",
+    "rounded-full",
+  ].join(" ");
   return (
     <div class="w-full flex items-center mb-4">
       <div
         tabIndex={0}
-        class="bg-gray-300 rounded-full w-8 h-8 text-center font-mono flex-none"
+        class="flex-none w-10 h-10 relative group"
         title={props.author}
       >
-        {props.author[0]}
+        <div class={expandingAuthorCircleClasses}>
+          <span class="inline-block w-4 text-center font-bold">
+            {props.author[0]}
+          </span>
+          <span class="invisible group-focus-within:visible group-hover:visible">
+            {props.author.slice(1)}
+          </span>
+        </div>
       </div>
-      <div class="relative bg-gray-200 rounded-lg shrink m-2 p-2">
+      <div class="bg-gray-200 rounded-lg shrink m-2 p-2">
         <div class="text-sm text-center">
           {formatter.format(props.published)}
         </div>
