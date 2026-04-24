b9f0d429f719e388ab83cf4d990dde1f4982c30c
Tims777 <git@tisizi.de>
2026-04-24 11:07:20 +0200

    Configure ahead-of-time builds properly

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-
 import "@std/dotenv/load";
+
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./helpers/twind-helpers.ts";
-
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, config);
